a4f0b23d80c9c86e47d1c663fd02f9a5be3843e2
zhubenjamin <benjaminzhu2020@gmail.com>
2025-10-10 12:37:29 -0400

    add optical sensor declaration on port 8

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,9 @@ pros::MotorGroup rightMotors({3, 5, -7}, pros::MotorGearset::blue); // right mot
 
 //pros::MotorGroup intake({11, 10}, pros::MotorGearset::blue);
 
+// optical sensor (no port determined yet)
+pros::optical::Optical optical_sensor(8)
+
 // Inertial Sensor on port 10
 pros::Imu imu(10);
 
