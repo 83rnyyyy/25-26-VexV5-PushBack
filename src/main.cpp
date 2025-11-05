199bcdc0b19b54a3f9d453e7c664feb0dc78170e
dave <newdevdave@gmail.com>
2025-11-05 13:06:16 -0500

    prepare for PID calibration

--- src/main.cpp
+++ src/main.cpp
@@ -45,26 +45,26 @@ lemlib::Drivetrain drivetrain(&leftMotors, // left motor group
 
 
 // lateral motion controller
-lemlib::ControllerSettings linearController(50, // proportional gain (kP)
-                                            5, // integral gain (kI)
-                                            15, // derivative gain (kD)
-                                            3, // anti windup
-                                            1, // small error range, in inches
-                                            100, // small error range timeout, in milliseconds
-                                            3, // large error range, in inches
-                                            500, // large error range timeout, in milliseconds
-                                            20 // maximum acceleration (slew)
+lemlib::ControllerSettings linearController(1, // proportional gain (kP)
+                                            0, // integral gain (kI)
+                                            3, // derivative gain (kD)
+                                            0, // anti windup
+                                            0, // small error range, in inches
+                                            0, // small error range timeout, in milliseconds
+                                            0, // large error range, in inches
+                                            0, // large error range timeout, in milliseconds
+                                            0 // maximum acceleration (slew)
 );
 
 // angular motion controller
-lemlib::ControllerSettings angularController(10, // proportional gain (kP)
-                                             5, // integral gain (kI)
-                                             50, // derivative gain (kD)
-                                             3, // anti windup
-                                             1, // small error range, in degrees
-                                             100, // small error range timeout, in milliseconds
-                                             3, // large error range, in degrees
-                                             500, // large error range timeout, in milliseconds
+lemlib::ControllerSettings angularController(1, // proportional gain (kP)
+                                             0, // integral gain (kI)
+                                             3, // derivative gain (kD)
+                                             0, // anti windup
+                                             0, // small error range, in degrees
+                                             0, // small error range timeout, in milliseconds
+                                             0, // large error range, in degrees
+                                             0, // large error range timeout, in milliseconds
                                              0 // maximum acceleration (slew)
 );
 
@@ -174,6 +174,11 @@ ASSET(example_txt); // '.' replaced with "_" to make c++ happy
  * This is an example autonomous routine which demonstrates a lot of the features LemLib has to offer
  */
 void autonomous() {
+    
+    // test movement for calibrating PID
+    chassis.setPose(0,0,0);
+    chassis.moveToPoint(0, 10, 9999);
+
     
     // // set position to x:0, y:0, heading:0
     // clamp.retract();
