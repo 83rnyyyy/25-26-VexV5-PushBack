ccf290fbd3f0e9fce7761e8e60c5b46206fd595d
zhubenjamin <benjaminzhu2020@gmail.com>
2026-02-07 17:37:03 -0500

    add pneumatics to driver control

--- src/main.cpp
+++ src/main.cpp
@@ -287,6 +287,8 @@ void autonomous() {
 bool manual = true;
 bool feederExtended = false;
 bool wingExtended = false;
+bool stopperExtended = false;
+bool togglerExtended = false;
 bool driveDirection = true; // default direction, brain side
 int yModifer = driveDirection ? 1 : -1;
 void opcontrol() {
@@ -295,7 +297,7 @@ void opcontrol() {
 
     while (true) {
         int leftY = yModifer*controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-        int rightX = yModifer*controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
+        int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
         chassis.arcade(leftY, rightX);
 
         // toggle manual/auto (debounced)
@@ -318,7 +320,7 @@ void opcontrol() {
         }
 
         // PNEUMATICS
-        if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_UP)) {
+        if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R2)) {
             wingExtended = !wingExtended;
             if (wingExtended) {
                 wing.extend();
@@ -332,6 +334,22 @@ void opcontrol() {
                 feeder.extend();
             } else {
                 feeder.retract();
+            }
+        }
+        if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R2)) {
+            stopperExtended = !stopperExtended;
+            if (stopperExtended) {
+                stopper.extend();
+            } else {
+                stopper.retract();
+            }
+        }
+        if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_UP)) {
+            togglerExtended = !togglerExtended;
+            if (togglerExtended) {
+                toggler.extend();
+            } else {
+                toggler.retract();
             }
         }
 
